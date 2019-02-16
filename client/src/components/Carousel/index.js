a25a21ebd58d0b39359127c8c5efd788478d1059
Sylvain COMBRAQUE <sylvain@les-tilleuls.coop>
2019-02-16 17:44:48 +0100

    Secure images

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -3,7 +3,7 @@ import "./carousel.css";
 import {Carousel as CarouselAddon} from 'react-responsive-carousel';
 
 const images = [
-  'http://www.angelio.net/blogangelio/wp-content/uploads/2018/08/application-rencontre-libido-810x400.jpg',
+  'https://www.angelio.net/blogangelio/wp-content/uploads/2018/08/application-rencontre-libido-810x400.jpg',
   'https://burst.shopifycdn.com/photos/nightclub-crowd-smoke-machine_4460x4460.jpg',
   'https://burst.shopifycdn.com/photos/man-adjusts-blue-tuxedo-bowtie_4460x4460.jpg',
   'https://burst.shopifycdn.com/photos/friends-taking-selfie_4460x4460.jpg',
